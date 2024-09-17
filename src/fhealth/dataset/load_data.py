418028ff85909e2a2cd11bda99467d7c7c9ae3a1
hadrien moncomble <hmoncomble@gmail.com>
2024-09-17 16:47:31 +0200

    load_data.py todo

---
+++ src/fhealth/dataset/load_data.py
@@ -0,0 +1,42 @@
+def load_data_with_cache():
+    """"""
+
+    # Verify if the local data files exists and are filled:
+
+    # If not, create the local data files according to its json template
+
+    # If yes, exit the function with an informative message
+
+    # Download the csv metadata files as one df
+
+    # Iterate over the metadata df:
+
+    # Extract image data:
+
+    # Construct the rgb image blob path according to its json template and file name in metadata
+
+    # Download the rgb image
+
+    # Construct the mask blob path according to its json template and file name in metadata
+
+    # Download the mask polygons
+
+    # Set the data status according to the file metadata
+
+    # Transform the image data:
+
+    # Create the mask image from polygons
+
+    # Create the blended rgb image with mask
+
+    # Set the data status according to the file metadata
+
+    # Store the image data:
+
+    # Construct the rgb image local data file path according to its json template and its data status
+
+    # Store the rgb image with its local data file path
+
+    # Construct the mask image local data file path according to its json template and its data status
+
+    # Store the mask image with its local data file path
