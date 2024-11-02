80a381d291057db85e52ea4d50623ecae5a23f18
hadrien moncomble <hmoncomble@gmail.com>
2024-11-02 17:32:47 +0100

    Write docstrinngs for the ForestHealthDataset object

--- src/fhealth/modeling/dataset.py
+++ src/fhealth/modeling/dataset.py
@@ -1,9 +1,10 @@
 import pathlib
-from typing import Any
 
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
+from torchvision.transforms import Lambda
 
 from fhealth.dataset.load_data import load_data_in_cache
 from fhealth.params import (
@@ -15,33 +16,71 @@ from fhealth.params import (
 
 
 class ForestHealthDataset(Dataset):
+    """
+    An object representing one of the three splits ('train', 'valid', 'test') of the Forest Health Dataset.
+    This object allow the torch.utils.data.Dataloader to load randomly datas into a training loop.
+    """
 
     def __init__(
         self,
         data_status: str,
         download: bool,
-        store_resolution: int | None = None,
-        transform: Any | None = None,
-        target_transform: Any | None = None,
-    ):
-        """"""
+        store_resolution: tuple | None = None,
+        transform: Lambda | None = None,
+        target_transform: Lambda | None = None,
+    ) -> None:
+        """
+        Read the metadata.csv file to acquire the local image and mask folder path,
+        Set the transformation functions for the image and the mask and download
+        the full dataset if the download argument is set to 'True'.
+
+        Args:
+            datastatus (str): The data status among 'train', 'valid' and 'test'.
+            download (bool): Wether to download the full dataset from cloud provider.
+            store_resolution (tuple | None): The dimensions (height, lenght)of the example image and the mask
+                                             if you choose to download the full dataset.
+            transform (torchvision Lambda object | None): The runtime transformation you want to apply to the features (images)
+            target_transform (torchvision Lambda object | None): The runtime transformation you want to apply to the targets (masks)
+
+        Returns:
+            None
+        """
+        # Read the metadata.csv file to acquire the local image and mask folder path
         self.data_status = data_status
         self.data_path = pathlib.Path(LOCAL_DATA_FOLDERS[self.data_status])
         self.metadata = pd.read_csv(LOCAL_METADATA_PATH).query(
             f"data_status == {self.data_status}"
         )
+
+        # Transformation functions for the image and the mask
         self.transform = transform
         self.target_transform = target_transform
 
+        # Download the full dataset if needed
         if download:
-            load_data_in_cache(store_resolution)
+            load_data_in_cache(*store_resolution)
 
-    def __len__(self):
-        """"""
+    def __len__(self) -> int:
+        """
+        Compute the number of samples in the dataset.
+
+        Returns:
+            The number of sample data in the dataset.
+        """
         return len(self.metadata)
 
-    def __getitem__(self, index):
-        """"""
+    def __getitem__(self, index: int) -> torch.Tensor:
+        """
+        This method allow the torch.utils.data.Dataloader object to randomly image and mask into a training loop.
+        The method also apply transformations to the image and the mask if the corresponding arguments were provided
+        when the ForestHealthDataset was initialized.
+
+        Args:
+            index (int): The index of the couple image, mask to return.
+
+        Returns:
+            A tuple image, mask
+        """
         # Extract local image and label folder name
         image_label_local_folder = self.metadata.iloc[index, 5]
 
